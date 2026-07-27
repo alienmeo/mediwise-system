282adc50b9052c462948653b5336609a8ea4e7cb
alienmeo <tkt.shnb@gmail.com>
2026-07-27 21:59:18 +0700

    x

--- backend/routes/auth_routes.py
+++ backend/routes/auth_routes.py
@@ -3,14 +3,10 @@ from controllers.auth_controller import register_user, login_user
 
 auth_bp = Blueprint('auth_bp', __name__)
 
-@auth_bp.route('/register', methods=['POST', 'OPTIONS'])
+@auth_bp.route('/register', methods=['POST'])
 def register():
-    if request.method == 'OPTIONS':
-        return '', 200
     return register_user()
 
-@auth_bp.route('/login', methods=['POST', 'OPTIONS'])
+@auth_bp.route('/login', methods=['POST'])
 def login():
-    if request.method == 'OPTIONS':
-        return '', 200
     return login_user()
