bb30dc785847d45a3806b6955ee33e9bdb8afb66
alienmeo <tkt.shnb@gmail.com>
2026-07-27 21:29:27 +0700

    fix cors options 500 error

--- backend/app.py
+++ backend/app.py
@@ -12,7 +12,7 @@ from models.db_models import AssessmentHistory
 app = Flask(__name__)
 
 # Bật CORS cho phép toàn bộ phương thức và origin
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'your-secret-key-mediwise'
