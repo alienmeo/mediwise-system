183a496a123a35387ee127033190644c3eb2249d
alienmeo <tkt.shnb@gmail.com>
2026-07-28 14:14:40 +0700

    xoadulieu

--- backend/app.py
+++ backend/app.py
@@ -86,4 +86,6 @@ def handle_exception(e):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
+        AssessmentHistory.query.delete()
+        db.session.commit()
     app.run(debug=True, port=5000)
