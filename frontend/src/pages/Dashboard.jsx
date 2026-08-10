84411947fdd6b4502f3f343fff621d070e53edb1
alienmeo <tkt.shnb@gmail.com>
2026-08-10 17:33:31 +0700

    si

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -18,7 +18,7 @@ export default function Dashboard() {
           {/* Brand Logo Header */}
           <div className="flex items-center justify-start cursor-pointer w-full overflow-hidden -ml-4 pl-3" onClick={() => navigate('/')}>
             <img src={logoImg} alt="Aellergis Logo" className="h-16 w-auto object-contain shrink-0" />
-            <img src={brandTextImg} alt="Aellergis" className="h-12 w-auto object-contain max-w-[210px] shrink-0 -ml-2.5" />
+            <img src={brandTextImg} alt="Aellergis" className="h-12 w-auto object-contain max-w-[210px] shrink-0 -ml-3.5" />
           </div>
 
           {/* Navigation Buttons */}
